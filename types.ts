b8267a6bbda887734a73059ed8417f1317c965ae
Johneric Pangan <johneric.pangan@dubizzle.com>
2021-01-24 22:35:21 +0400

    Initial commit.

---
+++ types.ts
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+	Root: undefined;
+	NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+	Home: undefined;
+	Profile: undefined;
+};
+
+export type HomeParamList = {
+  HomeScreen: undefined;
+};
+
+export type ProfileParamList = {
+	ProfileScreen: undefined;
+};
