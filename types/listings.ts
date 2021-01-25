005028a26a0aaf017416101aa6844e516de34703
Johneric Pangan <johneric.pangan@dubizzle.com>
2021-01-25 13:38:27 +0400

    Listings with image and empty state.

--- types/listings.ts
+++ types/listings.ts
@@ -1,3 +1,12 @@
 import { SliceState } from './state';
 
 export interface ListingsState extends SliceState{}
+
+export interface Product {
+  category: string;
+  description: string;
+  id: number
+  image: string;
+  price: number;
+  title: string;
+}
