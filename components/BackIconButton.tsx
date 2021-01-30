180efdfea66ef7a13a6885d6f05f8f32fada830c
Johneric Pangan <johneric.pangan@dubizzle.com>
2021-01-30 13:25:59 +0400

    Language toggle without app reloading.

---
+++ components/BackIconButton.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { TouchableOpacity, TouchableOpacityProps, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import {Colors} from '../constants/theme';
+import { useNavigation } from '@react-navigation/native';
+import {useSiteDirection} from '../components/AppDirectionProvider'
+
+
+interface BackIconButtonProps extends TouchableOpacityProps {
+  iconProps?: Object;
+  navigation?: object;
+}
+
+const BackIconButton = ({ canGoBack, iconProps = {}, ...rest }: BackIconButtonProps) => {
+  const { isRtl } = useSiteDirection();
+  const navigation = useNavigation();
+
+  if(!canGoBack) {
+    return null;
+  }
+
+  const navBack = () => {
+    if (canGoBack) {
+      navigation.goBack();
+    }
+  }
+
+  const iconName = isRtl ? 'arrow-forward-circle-outline' : 'arrow-back-circle-outline';
+  return (
+    <TouchableOpacity {...rest} onPress={navBack}>
+      <Ionicons name={iconName} style={styles.root} size={32} color={Colors.Grease} />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    marginHorizontal: 16
+  }
+});
+
+export default BackIconButton;
